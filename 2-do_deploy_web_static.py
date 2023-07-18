7072a1a4b9ba47c0c056b7c7637fb006686fc205
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-07-18 10:30:14 +0000

    function documented

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,6 +17,8 @@ def do_pack():
     local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
 
 def do_deploy(archive_path):
+    """distributes an archive to your web servers"""
+
     if not exists(archive_path):
         return False
 
@@ -24,15 +26,15 @@ def do_deploy(archive_path):
         filename = archive_path.split("/")[-1]
         filename_no_ext = filename.split(".")[0]
         put(archive_path, "/tmp/{}".format(filename))
-        run("mkdir -p /data/web_static/releases/{}/".format(filename_no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(filename_no_ext))
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(filename, filename_no_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
+        run("sudo rm /tmp/{}".format(filename))
+        run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
             .format(filename_no_ext, filename_no_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(filename_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(filename_no_ext))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(filename_no_ext))
 
         print("New version deployed!")
