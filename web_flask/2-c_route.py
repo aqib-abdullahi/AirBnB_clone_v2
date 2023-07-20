d12d36683f367377a1ad17cb83cead4d0803486a
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-07-20 22:19:50 +0000

    functions documented

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,5 +16,10 @@ def hbnb():
     """
     return "HBNB"
 
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """display C followed by text variable"""
+    return "C " + text.replace('_', ' ')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
